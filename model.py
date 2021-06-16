2cb8ff565da2dad8205ff9f745cacdf629684330
Отец <father@father.com>
2021-06-16 21:10:02 +0300

    comments added

--- model.py
+++ model.py
@@ -47,7 +47,7 @@ class QTrainer:
 			reward = torch.unsqueeze(reward, 0)
 			done = (done, )
 
-		# 1: predicted Q values with current state
+		# predicted Q values with current state
 		pred = self.model(state)
 
 		target = pred.clone()
@@ -58,9 +58,8 @@ class QTrainer:
 
 			target[idx][torch.argmax(action[idx]).item()] = Q_new
 	
-		# 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
-		# pred.clone()
-		# preds[argmax(action)] = Q_new
+		# Bellman equation
+		# Q_new = r + y * max(next_predicted Q value) -> only do this if not done
 		self.optimizer.zero_grad()
 		loss = self.criterion(target, pred)
 		loss.backward()
